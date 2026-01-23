0c55d850a370f599794065aeb622c1dca290a76c
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-23 17:16:23 +0530

    routing path create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <h1>JobLinker</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
