de61f6630af199799e3c48b4c3a02485b47eb56f
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-25 11:53:51 +0530

    feat: enhance Error page with custom layout and image

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import error1 from "../assets/images/error.jpg";
 
 const Error = () => {
   return (
-    <div>
-      <h1>Error page</h1>
-      <Link to="/">back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <img src={error1} alt="not found" />
+        <h3>Ohh! page not found</h3>
+        <p>we can't seem to find the page you are looking for</p>
+        <Link to="/dashboard">back home</Link>
+      </div>
+    </Wrapper>
   );
 };
 
