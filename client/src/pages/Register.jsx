5c6aa59a75fcdf5bc36cbd08d026f769fe855fc3
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-26 09:38:07 +0530

    feat: enhance Register page with form inputs and styling

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">Login</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="chanuka" />
+        <FormRow type="text" name="lastname" defaultValue="randitha" />
+        <FormRow type="text" name="Location" defaultValue="laxapana" />
+        <FormRow type="email" name="email" defaultValue="chanuka@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="chanuka" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
