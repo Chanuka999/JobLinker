8bdea227f6e025508be3b92b7d2c70e7bca12811
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-30 20:30:55 +0530

    feat: add BigSideBar component with styled wrapper

---
+++ client/src/components/BigSideBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/BigSidebar";
+
+const BigSideBar = () => {
+  return <Wrapper>Bigside bar</Wrapper>;
+};
+
+export default BigSideBar;
