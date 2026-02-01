79af062ea4915d02753071e35d8d31869ddb890c
Chanuka999 <chanukaranditha99@gmail.com>
2026-02-01 13:33:59 +0530

    feat: implement initial server setup with Express.js

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data recieved", data: req.body });
+});
+
+app.listen(5100, () => {
+  console.log("server sarting");
+});
