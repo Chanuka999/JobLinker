337a44494639cceaae93da7722d8f4c52735e064
Chanuka999 <chanukaranditha99@gmail.com>
2026-02-07 14:53:13 +0530

    feat: add Job model with schema definition for job listings

---
+++ server/models/JobModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const JobSchema = mongoose.Schema(
+  {
+    comapany: String,
+    position: String,
+    jobStatus: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    jobType: {
+      type: String,
+      enum: ["full-time", "part-time", "internship"],
+      default: "full-time",
+    },
+    jobLocation: {
+      type: String,
+      default: "my-city",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const jobModel = mongoose.model("Job", JobSchema);
+
+export default jobModel;
